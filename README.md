4553e9be812b7ce0d7558e6793c95951db3a9a0f
Bryce Spraggins <spragbry93@gmail.com>
2021-06-14 09:50:36 -0400

    starter code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # portfolio
+
+# Description 
+
+this is my personal portfolio website that has projects, contact info, and much more
+
+#Screen shot
+
+<img width="1321" alt="portfolio" src="https://user-images.githubusercontent.com/77589691/109581534-70d63100-7aca-11eb-81ad-e2cd122f749b.png">
+
+# Links
---
+++ portfolio.js
@@ -0,0 +1,16 @@
+window.onload = function(){ 
+    document.getElementById('aboutMe').onclick = function() {
+        document.getElementById("aboutMeDiv").scrollIntoView()
+    };
+    document.getElementById('projects').onclick = function() {
+        document.getElementById("projectDiv").scrollIntoView()
+    };
+    document.getElementById('contact').onclick = function() {
+        document.getElementById("contactResumeDiv").scrollIntoView()
+    };
+    document.getElementById('resume').onclick = function() {
+        document.getElementById("contactResumeDiv").scrollIntoView()
+    };
+};
+
+    
